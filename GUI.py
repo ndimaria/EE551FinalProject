71b4f45184e8d847c6630d1fd6f1c84c667c3f47
Nicholas DiMaria <ndimaria@stevens.edu>
2019-11-04 22:25:33 -0500

    Add Listbox

--- GUI.py
+++ GUI.py
@@ -1,12 +1,28 @@
 import tkinter as tk
 import FinalProject as FP
 from tkinter import StringVar, IntVar
+import pandas as pd
 
 root = tk.Tk()
 root.title('Stock Market')
 def change():
     stock_ticker = e1.get()
+    data = FP.searchWebsite(stock_ticker)
+
+    if isinstance(data, pd.core.frame.DataFrame):
+        Lb = tk.Listbox()
+        for index, row in data.iterrows():
+            Lb.insert(index, row['Name'] + " " + row['Symbol'])
+            print(row['Name'], row['Symbol'])
+        Lb.pack()
+    else:
+        lb2.config(text = data)
+"""
+def func(event):
+    stock_ticker = e1.get()
     lb2.config(text = FP.searchWebsite(stock_ticker))
+root.bind('<Return>', func)
+"""
 
 lb1 = tk.Label(root, text='Enter stock ticker:',width = 50)
 lb1.pack()
