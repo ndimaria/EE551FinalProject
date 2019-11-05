e2492a7a36436b1bcbcfa742b90d6a8b52d43304
Nicholas DiMaria <ndimaria@stevens.edu>
2019-11-04 22:01:51 -0500

    Add Entry box

--- GUI.py
+++ GUI.py
@@ -3,17 +3,20 @@ import FinalProject as FP
 from tkinter import StringVar, IntVar
 
 root = tk.Tk()
-
-text = StringVar()
-text.set('old')
-status = IntVar()
-
+root.title('Stock Market')
 def change():
-    lb.config(text = FP.searchWebsite("AAPL"))
+    stock_ticker = e1.get()
+    lb2.config(text = FP.searchWebsite(stock_ticker))
 
-cb = tk.Button(root, text = "OK", command=change)
-lb = tk.Label(root, width=50, text="Old Text")
+lb1 = tk.Label(root, text='Enter stock ticker:',width = 50)
+lb1.pack()
+e1 = tk.Entry(root)
+e1.pack()
+
+cb = tk.Button(root, text = "OK", command=change, justify ='center')
 cb.pack()
-lb.pack()
+
+lb2 = tk.Label(root, text = "Stock data will display here.", justify='center',width = 50)
+lb2.pack()
 
 root.mainloop()
