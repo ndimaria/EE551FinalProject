1d7cf099d978b1e0a52506af2f92591f8f61dcfe
Nicholas DiMaria <ndimaria@stevens.edu>
2019-11-16 00:12:11 -0500

    Add tests

---
+++ tests.py
@@ -0,0 +1,9 @@
+import FinalProject as FP
+import pandas as pd
+
+def test_fetch():
+    data = FP.searchWebsite("AAPL").split()
+    assert  data == ["The", "price", "of", "Apple", "(AAPL)", "is"]
+def test_search():
+    data = FP.searchWebsite("Apple")
+    assert isinstance(data, pd.core.frame.DataFrame)
